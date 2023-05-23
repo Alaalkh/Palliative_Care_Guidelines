7369ff5ac50506a0ee2b66d4f312ec66f12f428c
Alaa Alkhatib <aloo2ae@gmail.com>
2023-05-23 11:53:48 +0300

    Modify AndroidMinutest.xml

--- app/src/main/java/com/example/palliativecareguidelines/Fragments/ContactFragment2.java
+++ app/src/main/java/com/example/palliativecareguidelines/Fragments/ContactFragment2.java
@@ -39,7 +39,7 @@ public class ContactFragment2 extends Fragment {
         // Required empty public constructor
     }
 
-//////////////////////
+///////////////////////////
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
